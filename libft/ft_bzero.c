8a228f54808e63efe47eace962c79bf6868c5cc2
Tebourbi Sofien <104938050+SosoLeJovial@users.noreply.github.com>
2023-11-09 14:14:45 +0100

    fbzero done

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,34 +6,31 @@
 /*   By: soso <soso@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 17:52:53 by soso              #+#    #+#             */
-/*   Updated: 2023/11/08 18:14:13 by soso             ###   ########.fr       */
+/*   Updated: 2023/11/09 14:13:14 by soso             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void    *ft_bzero(void *s, size_t n)
 {
     unsigned char *str;
-    int i;
     
     str = (unsigned char *)s;
-    i = 0;
-    while (i < n)
+    while (n--)
     {
-        str[i] = 0;
+        *str = 48;
         str++;
     }
 }
 
 #include <stdio.h>
 #include <string.h>
-#include <stddef.h>
 
 int main()
 {
     char s1[] = "true story";
     char s2[] = "love story";
 
-    ft_bzero(s1, sizeof(s1));
+    ft_bzero(s1, 3);
     bzero(s2, sizeof(s2));
 
     printf("%s\n%s", s1, s2);
