9de847d4a9d7f4dc1fdc8681f385778d4dc1fbdd
SosoLeJovial <sofien.tebourbi@gmail.com>
2023-11-10 22:20:17 +0100

    atoi

---
+++ libft/ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 17:30:59 by tsofien-          #+#    #+#             */
+/*   Updated: 2023/11/10 22:18:17 by tsofien-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+// #include <limits.h>
+// #include <stdlib.h>
+
+int	ft_atoi(const char *nptr)
+{
+	int		i;
+	int		res;
+	int		sign;
+
+	sign = 1;
+	res = 0;
+	i = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	while (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] >= 48 && nptr[i] <= 57)
+	{
+		res = res * 10 + (nptr[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
+/*
+int	main(void)
+{
+	printf("%d | %d", ft_atoi("9223372036854775807"), atoi("9223372036854775807"));
+	return (0);
+}*/
+
