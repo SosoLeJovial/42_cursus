82bf7ea05830eb99fadb3bdebbfb9b120967e9f5
SosoLeJovial <sofien.tebourbi@gmail.com>
2023-11-10 14:24:29 +0100

    gneu pose clope

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,38 +6,39 @@
 /*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:43:04 by tsofien-          #+#    #+#             */
-/*   Updated: 2023/11/10 11:06:01 by tsofien-         ###   ########.fr       */
+/*   Updated: 2023/11/10 11:54:09 by tsofien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
 
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
     unsigned char *str;
     unsigned char *str1;
-    int i;
+
     str = (unsigned char *) s1;
     str1 = (unsigned char *) s2;
-    i = 0;
-    while (i < n)
+    while (n--)
     {
-        if (str[i] != str1[i])
-            return (str[i] - str1[i]);
-        i++;
+        if (str != str1)
+            return (*str - *str1);
+        str++;
+        str1++;
     }
     
     return (0);
 }
 
-/*
-#include <stdio.h>
-#include <string.h>
+
+
 int main()
 {
-    char s1[] = "ceci est un test";
-    char s2[] = "ceci n'est pas un test";
+    char s1[] = "a";
+    char s2[] = "a";
     
-    printf("%d", ft_memcmp(s1, s2, sizeof(s1)));
-    printf("%d", ft_memcmp(s1, s2, sizeof(s1)));
+    printf("%d\n", memcmp(s1, s2, sizeof(s1)));
+    printf("%d\n", ft_memcmp(s1, s2, sizeof(s1)));
     return (0);
-}*/
+}
---
+++ libft/ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 11:10:55 by tsofien-          #+#    #+#             */
+/*   Updated: 2023/11/10 11:19:57 by tsofien-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void    *memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char   *sorc;
+    unsigned char   *dst;
+    
+    sorc = (unsigned char *) src;
+    dst = (unsigned char *) dst;
+    while (n--)
+    {
+        dst = sorc;
+        dst++;
+        sorc++;
+    }
+    return (dst);
+}
