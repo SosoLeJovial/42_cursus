956bcbc37d87472eafbb7dc96d9e93ac6fe30007
SosoLeJovial <sofien.tebourbi@gmail.com>
2024-01-20 04:41:15 +0100

    born2berootdone

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,27 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/30 20:58:07 by tsofien-          #+#    #+#             */
-/*   Updated: 2024/01/20 04:40:05 by tsofien-         ###   ########.fr       */
+/*   Created: 2024/01/20 04:24:03 by tsofien-          #+#    #+#             */
+/*   Updated: 2024/01/20 04:37:06 by tsofien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 64 000 000 000 000 000 000
-# endif
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
 
-char	*get_next_line(int fd);
-int		ft_strlen(char *len);
-void	*ft_memmove(void *dest, const void *src, size_t len);
-#endif
+int	ft_printf(const char *format, va_args);
+
+int	main(void)
+{
+	ft_printf();
+	printf();
+	return (0);
+}
