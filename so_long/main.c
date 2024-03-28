6a66f12e7130fa2e5fd423c95e6b4b8d2a8263b6
SosoLeJovial <sofien.tebourbi@gmail.com>
2024-03-28 14:07:45 +0100

    start so long

---
+++ so_long/main.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsofien- <tsofien-@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 18:38:54 by tsofien-          #+#    #+#             */
+/*   Updated: 2024/03/27 21:25:31 by tsofien-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/so_long.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+
+// open, close, read, write,
+// malloc, free, perror,
+// strerror, exit
+// • Toutes les fonctions de la
+// bibliothèque mathématique
+// (option de compilation -lm,
+// man man 3 math)
+// • Toutes les fonctions de la
+// MiniLibX
+// • ft_printf et tout équivalent
+// que VOUS avez codé
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 1
+	if (ac < 2)
+		return (0);
+	while (i < ac)
+		printf("%s\n", av[i++]);
+	return (0);
+}
